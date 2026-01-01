bcc76a888b2578a3346ca05753468ac57893b1f9
LarsDepuydt <lars.depuydt@gmail.com>
2026-01-01 18:26:20 +0100

    day 1 - challenge 1

---
+++ day1/puzzle1.rs
@@ -0,0 +1,49 @@
+use std::io;
+
+fn parse_input() -> Vec<(char, i32)> {
+    let mut movements: Vec<(char, i32)> = Vec::new();
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input.");
+
+        let trimmed = input.trim();
+        if trimmed.is_empty() {
+            break;
+        }
+
+        let direction = trimmed.chars().next().unwrap();
+        let value: i32 = trimmed[1..].parse().expect("Invalid number");
+
+        movements.push((direction, value));
+    }
+
+    return movements;
+}
+
+fn main() {
+    println!("Input the given lock combination (Press Enter on an empty line to finish):");
+
+    const START_POSITION: i32 = 50;
+    let movements = parse_input();
+
+    let mut current_position = START_POSITION;
+    let mut password = 0;
+    for (direction, value) in &movements {
+        let new_position = if *direction == 'L' {
+            (current_position - value).rem_euclid(100)
+        } else {
+            (current_position + value).rem_euclid(100)
+        };
+
+        if new_position == 0 {
+            password = password + 1;
+        }
+
+        current_position = new_position;
+    }
+
+    println!("The password is: {password}")
+}
